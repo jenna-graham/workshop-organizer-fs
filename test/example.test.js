9001bf2025d8ccb4a503f9385acfefac9fce03d3
jenna-graham <jclayto2@gmail.com>
2022-05-24 10:25:02 -0700

    successful TDD of render function

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,21 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-
+import { renderWorkshop } from '../render-utils.js';
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('renderWorkshop', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = `<div class="workshop"><h2>Pottery 101</h2></div>`;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderWorkshop({
+        id: 1,
+        name: 'Pottery 101',   
+    });
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected);
 });
