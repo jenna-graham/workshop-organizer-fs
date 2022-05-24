aa21f7472f14273e11e2b7f8bab4767e593cbd4c
jenna-graham <jclayto2@gmail.com>
2022-05-24 12:05:39 -0700

    added event listener to new participant form

--- new-post/new-post.js
+++ new-post/new-post.js
@@ -12,3 +12,16 @@ async function loadData() {
     }
 }
 loadData();
+
+participantForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const form = new FormData(participantForm);
+    console.log(form.get('workshop_id'));
+    await createParticipant({
+        name: form.get('name'),
+        contact_info: form.get('contact'),
+        workshop_id: form.get('workshop_id'),
+    });
+    window.location.href = '/workshops';
+    
+});
